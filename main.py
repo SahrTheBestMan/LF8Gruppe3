d79a20f1f40f550bbcac26a3b3dcd957075bc8c6
fwippermann <flynnwippermann@gmail.com>
2025-04-09 14:19:39 +0200

    Fixing small inacuracies

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
+"""Hauptmodul zur Steuerung der Systemüberwachung"""
+from datetime import datetime
 from monitoring.ram_monitor import get_memory_usage, get_disk_usage
 from monitoring.cpu_monitor import get_cpu_usage
 from monitoring.alert import check_threshold
-from datetime import datetime
 
 LOG_FILE = "requirements.txt"  # Speichert die Werte in requiremnts
 
 def save_results(cpu, ram, disk):
     """Speichert die aktuellen Werte mit Datum in requirements.txt"""
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(LOG_FILE, "a") as file:  # "a" = an Datei anhängen
+    with open(LOG_FILE, "a", encoding="utf-8") as file:  # "a" = an Datei anhängen
         file.write(f"{timestamp} | CPU: {cpu:.1f}% | RAM: {ram:.1f}% | Festplatte: {disk:.1f}%\n")
 
 def main():
+    """Main Funktion"""
     print("System Monitoring gestartet...")
-    
+
     # Werte abfragen
     cpu = get_cpu_usage()
     ram = get_memory_usage()
     disk = get_disk_usage()
     
     # Ausgabe
-    print(f"\nAktuelle Werte:")
-    print(f"CPU: {cpu:.2f}%")
-    print(f"RAM: {ram:.2f}%")
-    print(f"Festplatte: {disk:.2f}%")
+    print(f"🔸 CPU       : {cpu:.2f}%")
+    print(f"🔸 RAM       : {ram:.2f}%")
+    print(f"🔸 Festplatte: {disk:.2f}%")
 
     # Werte speichern
     save_results(cpu, ram, disk)
